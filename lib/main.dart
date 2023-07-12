41ad71f3ae145650179c58cce6875151d21c7774
SumitSojitra <sumitsojitra533@gmail.com>
2023-07-12 10:56:38 +0530

    2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sky_scrapper_api_flutter_app/views/screens/SplashScreen.dart';
 import 'package:sky_scrapper_api_flutter_app/views/screens/home_page.dart';
 
 void main() {
@@ -6,8 +7,10 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
+      initialRoute: "splash_screen",
       routes: {
-        '/': (context) => HomePage(),
+        '/': (context) => const HomePage(),
+        'splash_screen': (context) => const SplashScreen(),
       },
     ),
   );
---
+++ lib/views/screens/SplashScreen.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 4), () {
+      Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 300,
+          width: 300,
+          decoration: BoxDecoration(
+              image:
+                  DecorationImage(image: AssetImage("assets/images/e.jpeg"))),
+        ),
+      ),
+    );
+  }
+}
